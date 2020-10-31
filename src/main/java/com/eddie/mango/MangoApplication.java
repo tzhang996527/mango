65f3c7186606aadaedd1ecd3fa62dd88aa7708ba
tzhang996527 <tzhang996527@hotmail.com>
2020-10-31 10:36:34 +0800

    111

--- src/main/java/com/eddie/mango/MangoApplication.java
+++ src/main/java/com/eddie/mango/MangoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MangoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MangoApplication.class, args);
+		SpringApplication.run(MangoApplication.class, args);//
 	}
 
 }
